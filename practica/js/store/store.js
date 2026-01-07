ed2eeef3d7f78e8d73c406036c46ef78ab64f354
Anais <anais.planas.91@gmail.com>
2026-01-07 17:05:33 +0100

    Store con listeners/notify

--- practica/js/store/store.js
+++ practica/js/store/store.js
@@ -7,35 +7,57 @@ import { Article } from "../clases/Article";
  export class Store {
 
     /** @type {Article[]} items */
-
     #items
-
     /** @type {LocalStorage} storage */
-
     #storage
+    /** @type {Array<(items:Article[]) => void>} */
+    #listeners
 
 
-     /** @param {LocalStorage} storage  */
-
+     
+    /** @param {LocalStorage} storage  */
     constructor(storage) {
         this.#storage = storage;
         this.#items = this.#storage.getItems();
+        this.#listeners = []
     }
+
+    //LECTURA DE ESTADO
 
     getArticles(){
         return this.#items
     }
 
+    //SUBSCIPCION A CAMBIOS
+
+    /** @param {(items: Article[]) => void} listener  */
+    subscribe(listener){
+        this.#listeners.push(listener)
+    }
+
+    //NOTIFICACION DE CAMBIOS A LOS SUBSCIRPTORES
+
+    notify(){
+        for(const listener of this.#listeners){
+            listener(this.#items)
+        }
+    }
+
+
+    //MUTACIONES DE ESTADO
+
     /** @param {Article} item  */
     addArticle(item){
         this.#items.push(item)
         this.#storage.setItems(this.#items)
+        this.#persist()
     }
 
     /** @param {number} id */
     removeArticleById(id){
         this.#items = this.#items.filter(item => item.id !== id)
         this.#storage.setItems(this.#items)
+        this.#persist()
     }
 
     /** @param {number} id  */
@@ -45,13 +67,23 @@ import { Article } from "../clases/Article";
             article.toggle()
             this.#storage.setItems(this.#items)
         }
+        this.#persist()
 
     }
 
     clear(){
         this.#items = []
         this.#storage.clear()
+        this.notify()
+    }
+
+    //METODO PRIVADO 
+
+    #persist(){
+        this.#storage.setItems(this.#items)
+        this.notify()
     }
    
  }
+ 
  
