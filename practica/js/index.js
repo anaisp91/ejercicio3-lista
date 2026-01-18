61fadfc1aca3c8d86d0f86a7ab5461060d9ec21e
Anais <anais.planas.91@gmail.com>
2026-01-18 10:37:12 +0100

    feat: :zap: modificacion index.js con redux

--- practica/js/index.js
+++ practica/js/index.js
@@ -1,19 +1,30 @@
 //@ts-check
-import { Store } from "./store/store.js"
-import { LocalStorage } from "./clases/LocalStorage.js"
-import { FabricaArticles } from "./clases/Article.js"
+//import { Store } from "./store/store.js"
+import { createState, reducer } from "./store/redux-store.js"
 import { ShoppingListController } from "./controllers/ShoppingListControler.js"
+//import { LocalStorage } from "./clases/LocalStorage.js"
+//import { FabricaArticles } from "./clases/Article.js"
+
 
 
 document.addEventListener('DOMContentLoaded', onDOMLoaded)
 
 function onDOMLoaded() {
     
-const storage = new LocalStorage('shopping-list')
-const store = new Store(storage)
-const fabrica = new FabricaArticles()
-const controller = new ShoppingListController(store, fabrica)
+//const storage = new LocalStorage('shopping-list')
+//const store = new Store(storage)
+//const fabrica = new FabricaArticles()
+//const controller = new ShoppingListController(store, fabrica)
 
+//controller.init()
+
+//crear lla store Redux
+const store = createState(reducer)
+
+//creaer controller
+const controller = new ShoppingListController(store)
+
+//arranca app
 controller.init()
 
 }
