8656cda3fc5fe1ec93837f693a9222365b46bd05
Anais <anais.planas.91@gmail.com>
2026-01-18 12:05:11 +0100

    pathname server ejemplo añadido

--- server/server.js
+++ server/server.js
@@ -8,8 +8,56 @@ http.createServer(function server_onRequest (request, response) {
     console.log("Request for " + pathname + " received.");
 
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write("<h1>Hello World</h1>");
+    /*
+    // Router:
+
+    switch(pathname) {
+         case "/":
+            response.write(getPageLayout(getHomePage()));
+            break;
+        case "/form":
+            response-write(getPageLayout(getFormPage()));
+            break;
+        default:
+            response.write(getPageLayour(getNotFoundPage()))
+            break;
+}
+*/
     response.end();
 }).listen(process.env.PORT, process.env.IP);
 
 console.log('Server running at http://' + process.env.IP + ':' + process.env.PORT + '/');
+
+
+
+//FUNCIONES DE SWITCH PATA PATHNAME
+/*
+function getHormPage(page){
+    return `
+       <html>
+           <head>
+               <title>servidor node JS</title>
+               <style>
+                   nav ul {
+                       display: flex;
+                       list-style-type: none;
+                       gap: 20px
+                       }
+               </style>
+           </head>
+           <body>
+               ${page}
+           </body>
+       </html>
+       ';
+
+}
+
+function getFormPage(page){
+
+}
+
+function getNotFoundPage(page){
+
+}
+*/
